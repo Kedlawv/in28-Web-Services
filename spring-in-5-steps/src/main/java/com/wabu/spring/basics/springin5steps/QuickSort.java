2bea9805779b46eafae0771261c667f092cef13e
Kedlawv <waldek.bus@gmail.com>
2020-10-16 21:22:18 +0200

    Mark QuickSort as Component | Conflict with Autowiring in BinarySearchImpl -> two beans of type SortAlgorithm registered | Conflict resolved with @Primary on one of the classes

--- spring-in-5-steps/src/main/java/com/wabu/spring/basics/springin5steps/QuickSort.java
+++ spring-in-5-steps/src/main/java/com/wabu/spring/basics/springin5steps/QuickSort.java
@@ -1,5 +1,10 @@
 package com.wabu.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSort implements SortAlgorithm {
     public int[] sort(int[] numbers) {
         //Logic for Quick Sort
