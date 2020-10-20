f3badf7199cc11f17e0a02fbc8cfe701eaf5bc69
Kedlawv <waldek.bus@gmail.com>
2020-10-20 16:45:39 +0200

    Course Service (in memory Courses as List | Course bean | Course mapper method (temp I'm guessing) -> move Course to CourseDetails mapping logic out of the Endpoint

--- soap-course-management-J8/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndPoint.java
+++ soap-course-management-J8/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndPoint.java
@@ -3,6 +3,9 @@ package com.in28minutes.soap.webservices.soapcoursemanagement.soap;
 import com.in28minutes.courses.CourseDetails;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
+import com.in28minutes.soap.webservices.soapcoursemanagement.soap.beans.Course;
+import com.in28minutes.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -11,17 +14,24 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class CourseDetailsEndPoint {
 
+    @Autowired
+    CourseDetailsService service;
+
     @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        Course course = service.findById(request.getId());
+        return mapCourse(course);
+    }
 
+    private GetCourseDetailsResponse mapCourse(Course course) {
         CourseDetails courseDetails = new CourseDetails();
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
 
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("A wonderful course");
-        
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
+
         response.setCourseDetails(courseDetails);
         return response;
     }
