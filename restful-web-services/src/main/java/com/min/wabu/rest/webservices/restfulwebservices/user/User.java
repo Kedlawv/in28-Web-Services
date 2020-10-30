cdc504e087d2533a68ceae1a2a8516331a3fa28e
Kedlawv <waldek.bus@gmail.com>
2020-10-30 12:08:46 +0100

    Validation spring boot starter needs to be added explicitly to the pom for spring boot 2.3+ -> the course is using 2.0 | Add validation for creating a user -> @Validate for the endpoint method parameter | @Size and @Past for the fields in the User bean | @Override handleMethodArgumentNotValid method in CustomResponseEntityExceptionHandler for a custom message on MethodArgumentNotValidException

--- restful-web-services/src/main/java/com/min/wabu/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/min/wabu/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,17 @@
 package com.min.wabu.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+
+    @Size(min = 2, message = "Size must be at least 2")
     private String name;
+
+    @Past
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
