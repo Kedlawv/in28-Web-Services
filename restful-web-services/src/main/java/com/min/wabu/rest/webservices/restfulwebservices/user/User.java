deb899745f1eba275932a1a79c12d9ec1d88099e
Kedlawv <waldek.bus@gmail.com>
2020-11-02 16:06:05 +0100

    Add to Swagger documentation description of input and output data formats -> application/json, application/xml |  Add Swagger doc annotations to the User class fields -> @ApiModelProperty

--- restful-web-services/src/main/java/com/min/wabu/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/min/wabu/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,7 @@
 package com.min.wabu.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -9,9 +11,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Size must be at least 2")
+    @ApiModelProperty(notes = "Size must be at least 2")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Date has to be in the past")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
