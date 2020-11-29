833fb34b12b7323cae7a6a84badfa7d06ab8b1ca
Kedlawv <waldek.bus@gmail.com>
2020-11-29 13:39:59 +0100

    UserRepository -> extends JpaRepository<Entity,Id> -> Spring provides a CRUD implementations for the declared Entity type

---
+++ jpa-in-10-steps/src/main/java/com/wabu/in28/jpa/jpain10steps/UserRepository.java
@@ -0,0 +1,7 @@
+package com.wabu.in28.jpa.jpain10steps;
+
+import com.wabu.in28.jpa.jpain10steps.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
