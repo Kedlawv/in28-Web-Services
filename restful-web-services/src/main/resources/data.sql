d5828d3db1b5212fccabf76db4e3926d6825df22
Kedlawv <waldek.bus@gmail.com>
2020-12-01 17:11:44 +0100

    Post @Entity @ManyToOne -> User | First part of annotation refers to the Entity its declared in second to the field -> Many Posts to one User in Post class | @OneToMany(mappedBy = "user") on the List<Post> field will create a column in the post table referencing userId -> foreign key
    add few test posts via data.sql

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into user values(101, sysdate(), 'Adam');
 insert into user values(102, sysdate(), 'Ewa');
 insert into user values(103, sysdate(), 'Olaf');
+insert into user values(104, sysdate(), 'Waldek');
+
+insert into post values(201, 'First test post', 104);
+insert into post values(202, 'Second post just to have two posts on one user', 104);
+insert into post values(203, 'Hi Im Eva and I dont know how to escape single quotes lol', 102);
