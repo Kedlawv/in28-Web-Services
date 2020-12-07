e2a61d4530653ffe37120df6b2d3c29d576ed014
Kedlawv <waldek.bus@gmail.com>
2020-12-07 12:54:28 +0100

    JDBC | JPA | H2 | Web Springboot initial commit

---
+++ database-demo/src/test/java/com/wabu/in28/database/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wabu.in28.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
