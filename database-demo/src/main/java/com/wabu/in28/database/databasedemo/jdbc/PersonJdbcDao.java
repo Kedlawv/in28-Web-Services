e3f426341f3bb28c09640d75e36a9a2fdd2e45cc
Kedlawv <waldek.bus@gmail.com>
2020-12-09 16:08:02 +0100

    Custom RowMapper for Person as inner class of PersonDAO

--- database-demo/src/main/java/com/wabu/in28/database/databasedemo/jdbc/PersonJdbcDao.java
+++ database-demo/src/main/java/com/wabu/in28/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.wabu.in28.database.databasedemo.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,15 +18,29 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class PersonRowMapper implements RowMapper<Person> {
+
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+
+            return person;
+        }
+    }
+
     public List<Person> findAll() {
         return jdbcTemplate.query("select * from person",
-                new BeanPropertyRowMapper<>(Person.class));
+                new PersonRowMapper()); // custom row mapper
     }
 
     public Person findById(int id) {
         return jdbcTemplate.queryForObject("select * from person where id = ?",
                 new Object[]{id},
-                new BeanPropertyRowMapper<>(Person.class));
+                new BeanPropertyRowMapper<>(Person.class)); // default row mapper
     }
 
     public List<Person> findByLocation(String location) {
@@ -50,7 +67,7 @@ public class PersonJdbcDao {
                 new Timestamp(person.getBirthDate().getTime()));
     }
 
-    public int update(Person person){
+    public int update(Person person) {
         return jdbcTemplate.update("update person set name=?, location=?, birth_date=? where id=?",
                 person.getName(),
                 person.getLocation(),
