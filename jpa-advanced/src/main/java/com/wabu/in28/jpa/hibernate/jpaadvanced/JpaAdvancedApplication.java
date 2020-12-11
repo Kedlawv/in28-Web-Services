4a2c1e5d3c2489490feac6bfafc8ed9a4acad859
Kedlawv <waldek.bus@gmail.com>
2020-12-11 02:24:47 +0100

    Initial -> Springboot Web + JPA + H2 -> New project for the Advanced JPA part of the course

---
+++ jpa-advanced/src/main/java/com/wabu/in28/jpa/hibernate/jpaadvanced/JpaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.wabu.in28.jpa.hibernate.jpaadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancedApplication.class, args);
+	}
+
+}
---
+++ jpa-advanced/src/test/java/com/wabu/in28/jpa/hibernate/jpaadvanced/JpaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wabu.in28.jpa.hibernate.jpaadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
