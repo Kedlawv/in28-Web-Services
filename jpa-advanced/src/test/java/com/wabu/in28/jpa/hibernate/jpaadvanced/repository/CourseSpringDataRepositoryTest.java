73e1bc4d83d621fd326356906c7fb0ce66d0c9ac
Kedlawv <waldek.bus@gmail.com>
2020-12-18 04:05:56 +0100

    findAll(sort) example/test

--- jpa-advanced/src/test/java/com/wabu/in28/jpa/hibernate/jpaadvanced/repository/CourseSpringDataRepositoryTest.java
+++ jpa-advanced/src/test/java/com/wabu/in28/jpa/hibernate/jpaadvanced/repository/CourseSpringDataRepositoryTest.java
@@ -6,7 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -42,10 +46,32 @@ class CourseSpringDataRepositoryTest {
         logger.info("\n---Course => {}", course);
         course.setName("Testing... Updated");
         repository.save(course);
-        
+
         Optional<Course> actualCourse = repository.findById(course.getId());
         if(!actualCourse.isPresent()) { fail("Item not found!");}
         assertEquals("Testing... Updated", actualCourse.get().getName());
+    }
+
+    @Test
+    @DirtiesContext
+    public void sort(){
+        Sort sort = Sort.by(Sort.Direction.ASC, "name");
+        // Sort sort = new Sort(Sort.Direction.ASC, "name");
+        // ↑ Constructor for Sort has become protected since the course have been recorded
+        List<Course> expectedCourses = new ArrayList<>();
+        expectedCourses.add(new Course("C"));
+        expectedCourses.add(new Course("B"));
+        expectedCourses.add(new Course("A"));
+        repository.save(expectedCourses.get(0));
+        repository.save(expectedCourses.get(1));
+        repository.save(expectedCourses.get(2));
+
+        List<Course> actualCourses = repository.findAll(sort);
+        logger.info("\nCourses => {}", actualCourses);
+
+        assertEquals("A", actualCourses.get(0).getName());
+        assertEquals("B", actualCourses.get(1).getName());
+        assertEquals("C", actualCourses.get(2).getName());
     }
 
 
