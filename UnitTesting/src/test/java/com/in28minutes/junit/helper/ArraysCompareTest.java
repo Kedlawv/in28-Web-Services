e6c07d062966597ec4119bb395388cffe327a689
Kedlawv <waldek.bus@gmail.com>
2020-12-31 13:23:27 +0100

    assertArrayEquals()

---
+++ UnitTesting/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,21 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ArraysCompareTest {
+
+    @Test
+    public void testArraySort_RandomArray(){
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+        Arrays.sort(numbers); // sorts in place
+
+        assertArrayEquals(expected,numbers);
+        // will compare contents of the array instead of the object reference
+
+    }
+}
