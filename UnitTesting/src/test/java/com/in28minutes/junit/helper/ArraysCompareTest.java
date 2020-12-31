6c800cda270df37c9d4ca840084ade086649e59e
Kedlawv <waldek.bus@gmail.com>
2020-12-31 14:00:03 +0100

    @Timeout test example JUnit5

--- UnitTesting/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ UnitTesting/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -1,8 +1,11 @@
 package com.in28minutes.junit.helper;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +31,16 @@ public class ArraysCompareTest {
             Arrays.sort(numbers);
         });
         // takes the type of exception and an executable (lambda)
+    }
 
+    @Test
+    @Timeout(value=3, unit = TimeUnit.MILLISECONDS)
+    public void testArraySortPerformance() {
+        int[] array = new int[1000];
+        Random rand = new Random();
+        for(int i = 0; i<1000; i++){
+            array[i] = rand.nextInt(1000);
+        }
+        Arrays.sort(array);
     }
 }
